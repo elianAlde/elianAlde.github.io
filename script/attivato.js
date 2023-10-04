f4b7c47a8eca7c28430f64e868abaaf7557bc885
Elian Alderuccio <elian.alderuccio@outlook.it>
2023-10-05 00:20:36 +0200

    Layout card penso definitivo

--- script/attivato.js
+++ script/attivato.js
@@ -39,3 +39,19 @@ $(document).ready(function() {
         }
     });
 });
+
+
+//CODICE ANIMAZIONE COMPARSA CARDS
+//Effetto animazione caricamento contenuti a comparsa
+document.addEventListener("DOMContentLoaded", function() {
+    var contenutiDaMostrare = document.querySelectorAll(".comparsa");
+
+    contenutiDaMostrare.forEach(function(contenuto) {
+      // Mostra ciascun contenuto con l'effetto a comparsa
+    contenuto.style.display = "block";
+    setTimeout(function() {
+        contenuto.style.opacity = "1";
+        contenuto.style.transform = "translateY(0)";
+    }, 500);
+    });
+});
